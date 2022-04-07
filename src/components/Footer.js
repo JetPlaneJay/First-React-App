aaec3af9bb12b9f8042eab3264fd1754b44006ed
unknown <jacob.tabares90@gmail.com>
2022-04-06 18:47:45 -0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <footer>
+
+            <a href="https://www.facebook.com/bobby.fischer.907">
+                <i class='fa fa-facebook-official fa-3x'></i>
+            </a> 
+            <a href="https://www.instagram.com/_jaketabares_/">
+                <i class='fa fa-github-square fa-3x'></i>
+            </a>
+            <a href="https://github.com/JetPlaneJay">
+                <i class='fa fa-instagram fa-3x'></i>
+            </a> 
+        </footer>
+    )
+}
